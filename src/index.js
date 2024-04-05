b28b5a12e758cf1836e44acf475bd36fb0eec88c
Gulshan261197 <gulshankumar261197@gmail.com>
2024-04-05 14:13:57 +0530

    Add utils folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv"
 import connectDB from "./db/database.js"
+import express from "express"
+const app = express()
+
+
 
 dotenv.config({
     path: './env'
 })
 
-connectDB()
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server running on port  ${process.env.PORT}`);
+    })
+})
+    .catch((err) => {
+        console.log(`Database connection failed !!!`, err);
+    });
